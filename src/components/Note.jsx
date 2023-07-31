7d393e4c047e3ff3d1afa6a9ad5fedf046743c0a
Dominik <110234711+Gho2st@users.noreply.github.com>
2023-07-31 17:29:15 +0200

    Add files via upload

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return <div className="note">
-        <h1>Title of the note</h1>
-        <p>The content of the note</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 }
 
